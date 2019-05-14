ca14e699ae091ecb0f543f780ce40ae4c7e566e9
zuofei <348068347@qq.com>
2019-05-14 16:42:46 +0800

    加载网页

--- app/src/main/java/com/zuofei/webview/MainActivity.java
+++ app/src/main/java/com/zuofei/webview/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         WebView m_webview = findViewById(R.id.web_view_id);
-        m_webview.loadUrl("http://www.baidu.com");
+        m_webview.loadUrl("http://www.baidu.com"); //加载web网页
+        m_webview.setWebViewClient(new WebViewClient()); //在本应用中打开网页
     }
 }
