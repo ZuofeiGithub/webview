0b37abe4b7bb8cc6517a5950ec54f4fa6da1ca65
zuofei <348068347@qq.com>
2019-05-14 16:52:10 +0800

    网页返回

--- app/src/main/java/com/zuofei/webview/MainActivity.java
+++ app/src/main/java/com/zuofei/webview/MainActivity.java
@@ -3,17 +3,29 @@ package com.zuofei.webview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
-
+    WebView mWebview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView m_webview = findViewById(R.id.web_view_id);
-        m_webview.loadUrl("http://www.baidu.com"); //加载web网页
-        m_webview.setWebViewClient(new WebViewClient()); //在本应用中打开网页
+        mWebview = findViewById(R.id.web_view_id);
+        mWebview.loadUrl("http://www.baidu.com"); //加载web网页
+        mWebview.setWebViewClient(new WebViewClient()); //在本应用中打开网页
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == event.KEYCODE_BACK){
+            if(mWebview.canGoBack()){
+                mWebview.goBack();
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
